064c33db0f9dc9303a36573e63471225b1d5fb2d
ab <adam.babol@pl.ibm.com>
2023-01-25 16:45:37 +0100

    updates after review

--- pkg/sources/source_ilmt.go
+++ pkg/sources/source_ilmt.go
@@ -97,7 +97,10 @@ func (i *ilmtSource) Pull(
 	var buffer bytes.Buffer
 
 	// create archive file
-	Tar(tempDir, &buffer)
+	err = Tar(tempDir, &buffer)
+	if err != nil {
+		return 0, err
+	}
 
 	reportFileName := fmt.Sprintf("upload-ilmt-%s-%s.tar.gz", dateRangeOptions.StartDate, dateRangeOptions.EndDate)
 
@@ -148,11 +151,7 @@ func CreateFileFromString(outFile string, data string) error {
 	return nil
 }
 
-// Tar takes a source and variable writers and walks 'source' writing each file
-// found to the tar writer; the purpose for accepting multiple writers is to allow
-// for multiple outputs (for example a file, or md5 hash)
 func Tar(src string, writers ...io.Writer) error {
-	// ensure the src actually exists before trying to tar it
 	if _, err := os.Stat(src); err != nil {
 		return fmt.Errorf("unable to tar files - %v", err.Error())
 	}
@@ -165,46 +164,36 @@ func Tar(src string, writers ...io.Writer) error {
 	tw := tar.NewWriter(gzw)
 	defer tw.Close()
 
-	// walk path
 	return filepath.Walk(src, func(file string, fi os.FileInfo, errIn error) error {
 
-		// return on any error
 		if errIn != nil {
-			return errors.Wrap(errIn, "fail to tar files")
+			return errors.Wrap(errIn, "failed to tar files")
 		}
 
-		// return on non-regular files (thanks to [kumo](https://medium.com/@komuw/just-like-you-did-fbdd7df829d3) for this suggested update)
 		if !fi.Mode().IsRegular() {
 			return nil
 		}
 
-		// create a new dir/file header
 		header, err := tar.FileInfoHeader(fi, fi.Name())
 		if err != nil {
 			return errors.Wrap(err, "failed to create new dir")
 		}
 
-		// update the name to correctly reflect the desired destination when untaring
 		header.Name = strings.TrimPrefix(strings.ReplaceAll(file, src, ""), string(filepath.Separator))
 
-		// write the header
 		if err = tw.WriteHeader(header); err != nil {
 			return errors.Wrap(err, "failed to write header")
 		}
 
-		// open files for taring
 		f, err := os.Open(file)
 		if err != nil {
-			return errors.Wrap(err, "fail to open file for taring")
+			return errors.Wrap(err, "failed to open file for taring")
 		}
 
-		// copy file data into tar writer
 		if _, err := io.Copy(tw, f); err != nil {
 			return errors.Wrap(err, "failed to copy data")
 		}
 
-		// manually close here after each file operation; defering would cause each file close
-		// to wait until all operations have completed.
 		f.Close()
 
 		return nil
